cd2d71b419b393530bca396de8206fd6f02a9a4d
Kunal singh <159079538+Iamkumal29@users.noreply.github.com>
2024-02-17 10:50:28 +0000

    new configuration

---
+++ _01basic/Variables.js
@@ -0,0 +1,13 @@
+const accountid = 133356
+let accountemail = "kunalsingh@gmail.com"
+var accountpassword = 12345
+     accountcity = "delhi"
+let accountstate;
+
+     console.log("accountid");
+
+     accountemail = "kundansingh@gmail.com"
+     accountpassword = 1212121
+     accountcity = "mumbai"
+
+     console.table([accountid, accountemail, accountpassword, accountcity, accountstate])
