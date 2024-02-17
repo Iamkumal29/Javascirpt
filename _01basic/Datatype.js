3cda4aef7c855e16012679b0bab80576b88f4945
Kunal singh <159079538+Iamkumal29@users.noreply.github.com>
2024-02-17 11:15:23 +0000

    new file

---
+++ _01basic/Datatype.js
@@ -0,0 +1,21 @@
+// Data-types
+ 
+let name = "kunal" // string 
+let age = 20 // number
+let temperature = null  //null
+let  islogin = true //boolean 
+
+// Two types of data-type
+// 1. Primitive data-type
+// 2. object data-type
+
+//1. Primitive data-type
+// a. string
+// b. number  
+// c. boolean
+// d. null ( standalone value )
+// e. undefined
+// f. bigint ( it will use when we talk about larger number)
+// g. symbol ( uniquness)
+ 
+console.log(typeof age);
